781c1ac83041a4f13e8d4903a01598da593ceed1
adam ounai <ounaiadam@gmail.com>
2023-03-18 11:32:02 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -10,7 +10,6 @@
 
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-
 	write(1, quo, 59);
-	return (1);	
+	return (1);
 }
