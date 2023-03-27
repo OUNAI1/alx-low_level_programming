13d6dca424cac5e628e5ed2cc4ef1a8e86945ea0
adam ounai <ounaiadam@gmail.com>
2023-03-26 17:31:04 -0700

    i am late

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - print the alphabet from a to z in 10 lines
+ *
+ * Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int line = 0;
+	char ch = 'a';
+
+	for (line = 0; line <= 10; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
