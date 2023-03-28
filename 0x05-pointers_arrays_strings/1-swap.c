d503ddbe5bce06a1f1f9c66889766ff897c5c8fe
adam ounai <ounaiadam@gmail.com>
2023-03-28 09:51:50 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int glass;
+
+	*a = glass;
+	*b = *a;
+	glass = *b;
+}
