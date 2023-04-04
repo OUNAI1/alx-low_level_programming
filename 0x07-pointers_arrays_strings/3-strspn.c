58f89238fc7b6d78fed35d7e8e67bfe0d8169448
adam ounai <ounaiadam@gmail.com>
2023-04-04 10:03:26 -0700

    things

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+/**
+ * _strspn -  function that gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ *
+ * Return: the pointer to dest.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int var1 = 0, var2 = 0;
+
+	while (s[var1] != '\0')
+	{
+	while (accept[var2] != s[var1])
+	{
+		var2++;
+		if (accept[var2] == '\0')
+		{
+			return (var1);
+		}
+	}
+	var1++;
+	}
+	return (var1);
+}
