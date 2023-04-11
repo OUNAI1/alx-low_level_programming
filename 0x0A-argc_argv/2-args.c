15869ad56f947c84f53a97d4df651e4ef69cfec0
adam ounai <ounaiadam@gmail.com>
2023-04-10 19:26:00 -0700

    100 task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int iter;
+
+	for (iter = 0; i < argc; iter++)
+	{
+		printf("%s\n", argv[iter]);
+	}
+
+	return (0);
+}
+
