546d3d8e5d07f336caefeb82abb4c478c5f76c05
adam ounai <ounaiadam@gmail.com>
2023-04-14 16:51:29 -0700

    thing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *soo;
+	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
@@ -24,21 +24,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (!soo)
+	if (!s)
 		return (NULL);
 
 	while (i < len1)
 	{
-		soo[i] = s1[i];
+		s[i] = s1[i];
 		i++;
 	}
 
 	while (n < len2 && i < (len1 + n))
-		soo[i++] = s2[j++];
+		s[i++] = s2[j++];
 	while (n >= len2 && i < (len1 + len2))
-		soo[i++] = s2[j++];
+		s[i++] = s2[j++];
 
-	soo[i] = '\0';
+	s[i] = '\0';
 
-	return (soo);
+	return (s);
 }
