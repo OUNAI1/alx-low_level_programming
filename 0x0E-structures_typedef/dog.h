8bd8a22d769fd7125a05424a5dba53db0279cea6
adam ounai <ounaiadam@gmail.com>
2023-04-17 11:06:06 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dogs data
+ * @name: the dog name
+ * @age:dog's age
+ * @owner: dog's owner
+ *
+ * description: dog's structers
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
